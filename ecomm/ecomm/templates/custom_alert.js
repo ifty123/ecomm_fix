ed6b43e6e0dd2c5385056e177fbd47a68481627a
Alif_ita <alifipa5@gmail.com>
2023-06-02 22:45:28 +0700

    first commit

---
+++ ecomm/ecomm/templates/custom_alert.js
@@ -0,0 +1,9 @@
+mdb.Alert.getInstance(document.getElementById("custom-alert-base")).update({
+  position: "top-right",
+  delay: 2000,
+  autohide: false,
+  width: "600px",
+  offset: 20,
+  stacking: false,
+  appendToBody: false,
+});
